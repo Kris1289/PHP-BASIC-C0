aa8721bcbbafed556027aca01812bd10a86e9375
Kris <krdelver@gmail.com>
2021-12-15 17:56:27 +0100

    klaar

---
+++ 2-Variabelen-en-datatype/2.6 - gettype() 2/index.php
@@ -0,0 +1,30 @@
+<?php
+$score = 5;
+echo $score;
+?>
+
+<?php
+$x = "Kris";
+
+echo $x;
+echo "<br>";
+?>
+
+<?php
+$leeftijd = 17;
+echo $leeftijd;
+$waterprijs = 1,65;
+echo $waterprijs;
+?>
+
+<?php
+$test = true;
+echo "de waarde van test is: " . $test . "<br>";
+$test = null;
+echo "de waarde van test is: " . $test . "<br>";
+?>
+
+<?php
+$voornaam = "Kris";
+    echo gettype($voornaam);
+?>
