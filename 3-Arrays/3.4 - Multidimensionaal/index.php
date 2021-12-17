131538a4c14033ef72e185fa72b06aad55464e4b
Kris <krdelver@gmail.com>
2021-12-17 14:12:40 +0100

    3.4

---
+++ 3-Arrays/3.4 - Multidimensionaal/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$abcGetallen=array("A","B","C");
+$abcGetallen=array("24","56","78","90");
+print_r($abcGetallen);
+echo "a56","c90";
+?>
+</body>
+</html> 
