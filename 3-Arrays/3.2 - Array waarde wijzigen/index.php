107402ce7ef9de5ba660531bba161f140c46ea98
Kris <krdelver@gmail.com>
2021-12-17 14:02:05 +0100

    3.2

--- 3-Arrays/3.2 - Array waarde wijzigen/index.php
+++ 3-Arrays/3.2 - Array waarde wijzigen/index.php
@@ -2,12 +2,8 @@
 $myCatNames[0] =  'tijger';
 $myCatNames[1] =  'mickey';
 $myCatNames[2] =  'lotje';
-    echo $myCatNames[0];
-    echo $myCatNames[1];
-    echo $myCatNames[2];
+    echo "<li>$myCatNames[0]</li><li>$myCatNames[1]</li><li>$myCatNames[2]</li>";
 $myCatNames[0] = 'pompoen';
-    echo $myCatNames[0];
-    echo $myCatNames[1];
-    echo $myCatNames[2];
+    echo "<li>$myCatNames[0]</li><li>$myCatNames[1]</li><li>$myCatNames[2]</li>";
 
 ?>
