cf0874fadacd6838c6a73aa8559c4d6994c902a2
Kris <krdelver@gmail.com>
2021-12-23 00:01:31 +0100

    3.5

---
+++ 3-Arrays/3.5 - Array sorteren/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+$getallen=array("676","5345","78657","3","46","21","47");
+$laag_hoog=array("3","21","46","47","676","5345","78657");
+print_r($laag_hoog);
+
+$hoog_laag=array("78657","5345","676","47","46","21","3");
+print_r($hoog_laag);
+
+?>
+</body>
+</html>
