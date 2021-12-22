99215d70a1daf9ef54569db62659cfe45c53c355
Kris <krdelver@gmail.com>
2021-12-23 00:10:57 +0100

    4.1

---
+++ 4-Arithmetics/4.1 - Arithmetic Operators/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+$getal1 = 64;
+$getal2 = 2;
+    echo $getal1 + $getal2;
+    echo $getal1 - $getal2;
+    echo $getal1 / $getal2;
+    echo $getal1 * $getal2;
+
+?>
+</body>
+</html>
