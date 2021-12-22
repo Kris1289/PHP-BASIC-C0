f56314fbe2b2a8e1d14d86d1f57ddb0859aba84d
Kris <krdelver@gmail.com>
2021-12-23 00:34:16 +0100

    5.4

--- 5-Conditional_Statements/5.4- if, elseif, else conditonal/index.php
+++ 5-Conditional_Statements/5.4- if, elseif, else conditonal/index.php
@@ -4,7 +4,17 @@
 
 <?php
 
+$prod_besteld = 30;
 
+if($prod_besteld < 10) {
+    $prijs = 1.50;
+} 
+elseif($prod_besteld < 25) {
+    $prijs = 1.25;
+} 
+else {
+    $prijs = 1.00;
+}
 
 ?>
 </body>
