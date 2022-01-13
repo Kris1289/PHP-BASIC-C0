3e6e404197c62f435faac4d1db2e5e9c949474dc
Kris <krdelver@gmail.com>
2022-01-13 12:27:28 +0100

    Eindopdracht klaar

--- Eindopdracht/index.php
+++ Eindopdracht/index.php
@@ -14,6 +14,7 @@
 /**************************** De code hieronder heb je nodig en hoef je niet aan te passen *************************/
 ?>
 
+<?php include 'bitcoinData.php'; ?>
 <?php include 'header.php'; ?>
 <div class="container mt-4">
     <div class="row mb-4">
