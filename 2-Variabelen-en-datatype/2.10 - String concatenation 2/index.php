c016f4c337b80a1833b3c2c39f4240261af75be9
Kris <krdelver@gmail.com>
2022-01-17 23:44:44 +0100

    2.10

--- 2-Variabelen-en-datatype/2.10 - String concatenation 2/index.php
+++ 2-Variabelen-en-datatype/2.10 - String concatenation 2/index.php
@@ -1,17 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
 <?php
-
-$voornaam1 = "Suspect 1";
-$voornaam2 = "Suspect 2";
-$voornaam3 = "Suspect 3";
-
-echo "<li>$voornaam1</li><li>$voornaam2</li><li>$voornaam3</li>";
-
-
-$item = "<li>tas</li>";
-    $item = $item . "<li>bal</li>";
-
-    $item = "<li>tas</li>";
-    $item .= "<li>bal</li>";
-
-
+$item = "<li>Kris</li>";
+$item .= "<li>Kyllian</li>";
+$item .= "<li>Abdelrahman</li>";
+echo $item
 ?>
+
+</body>
+</html> 
